0ea6b3148882c05ba794822a2755e607dbfb9610
Matt Kleinberg <matt@rbm.tv>
2017-01-24 13:44:14 -0500

    working on stuff

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import tokens
+from critical_role.reddit import RedditBot
 
 
 client = discord.Client()
@@ -15,13 +16,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
-        print(message.timestamp)
+        RedditBot().get_time_till_cr(message.timestamp)
 
 @client.event
 async def on_voice_state_update(before, after):
-    if after.voice.voice_channel and before is None:
+    print(after.voice.voice_channel.is_private)
+    if after.voice.voice_channel and before.voice.voice_channel is None:
         server = after.server
         msg = str(after.display_name) + ' joined ' + after.voice.voice_channel.name
+        tmp = await client.send_message(server, msg, tts=True)
+        await client.delete_message(tmp)
+    elif after.voice.voice_channel is None:
+        server = before.server
+        msg = str(after.display_name) + ' left the server'
         tmp = await client.send_message(server, msg, tts=True)
         await client.delete_message(tmp)
 
